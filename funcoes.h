88f3bf599fe95f51b35ec2e35dbef53ba1531b04
Fernando Breno <fernandobrenofx@gmail.com>
2019-11-06 14:34:39 -0300

    Add files via upload

--- funcoes.h
+++ funcoes.h
@@ -60,6 +60,65 @@ void color(pixel p,unsigned short r, unsigned short g,unsigned short b){
     p.blue=b;
 }
 
+void line(pixel **p,reta r){ //https://gist.github.com/mfilipelino/11240714#file-paintopengl-c
+    int dy = (r.p2.y - r.p1.y); //Diferença entre os pontos y2 e y1
+    int dx = (r.p2.x - r.p1.x); //Diferença entre os pontos x2 e x1
+    int sx,sy; //Variaveis que serão utilizadas para armazenar o sinal de x e y afim de saber o quadrante do segmento
+    int i=r.p1.y;
+    int j=r.p1.x;
+    if(r.p1.x<r.p2.x) //Determinar os sinais de x e y
+    {
+        sx=1;
+    }
+    else
+    {
+        sx=-1;
+    }
+
+    if(r.p1.y<r.p2.y)
+    {
+        sy=1;
+    }
+    else
+    {
+        sy=-1;
+    }                  //fim da determinação de sinais
+    
+    int erro;
+    int erroTemp;
+    if(dx>dy) // Definição do erro inicial baseado na maior diferença
+    {
+        erro=dx/2;
+    }
+    else
+    {
+        erro=(-dy)/2;
+    }
+    
+    while(true)
+	{
+	    color(p[i][j],255,255,255);
+        if((i==r.p2.y)&&(j==r.p2.x)) // Testa se chegou ao ponto final e encerra o algoritimo
+        {
+            break;
+        }
+
+        erroTemp=erro;
+
+        if(erroTemp > -dx)
+        {
+            erro -= dy;
+            j += sx;
+        }
+        
+        if(erroTemp < dy)
+        {
+            erro += dx;
+            i += sy;
+        }
+	}
+}
+
 /*void clear(pixel **p,unsigned short r, unsigned short g,unsigned short b){ //Limpa a imagem setando todos os pixels para uma cor especificada
     for(int j=0;j<dimy;j++)
 	{
@@ -67,16 +126,7 @@ void color(pixel p,unsigned short r, unsigned short g,unsigned short b){
 		{
             color(p[i][j],r,g,b);	
 		}
-}
-
-void line(pixel **p,reta r){
-    for(int j=0;j<dimy;j++)
-	{
-		for(int i=0;i<dimx;i++)
-		{
-            color(p[i][j],r,g,b);	
-		}
-    }   
+    }
 }
 
 void rect(pixel **p){ //FUNÇÃO PARA DESENHAR RETÂNGULOS 
--- structs.h
+++ structs.h
@@ -1,22 +1,28 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-typedef struct ponto{ //Struct para armazenar as posições x e y de um ponto
+typedef struct{ //Struct para armazenar as posições x e y de um ponto
 
 int x , y;
 
 }ponto;
 
-typedef struct reta{ //Struct para armazenar 2 ou mais pontos para formarem uma reta
+typedef struct{ //Struct para armazenar 2 ou mais pontos para formarem uma reta
 
 ponto p1;
 ponto p2;
 
 }reta;
 
-typedef struct pixel{ //Struct para armazenar as cores no canvas
+typedef struct{ //Struct para armazenar as cores
      unsigned short red,green,blue;
 } pixel;
+
+enum Bool
+{
+    false = 0,
+    true = 1
+};
 
 #endif
 
