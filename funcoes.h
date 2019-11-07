a71c77ea083e1b53d680c16c094ac4a3fcee2eab
Fernando Breno <fernandobrenofx@gmail.com>
2019-11-07 14:55:36 -0300

    Add files via upload

--- funcoes.h
+++ funcoes.h
@@ -3,10 +3,11 @@
 
 int dimx=0,dimy=0;
 
-void abrir_arquivo(pixel **p,char w[50]){ //função para letitura do arquivo
+pixel** abrir_arquivo(char w[50]){ //função para letitura do arquivo
 	FILE *img = fopen(w, "r");
     fscanf(img, "P3\n%d %d\n255\n", &dimx, &dimy);
-
+    pixel **p;
+    
 	for(int i=0;i<dimx;i++)
     {
 		for(int j=0;j<dimy;j++)
@@ -24,10 +25,15 @@ void abrir_arquivo(pixel **p,char w[50]){ //função para letitura do arquivo
         fscanf(img,"%c", &t);
 	}
     fclose(img);
+    return p;
 }
 
-void image (int x,int y){ //Cria uma nova "imagem", com largura e altura especificadas
+
+pixel** image (int x,int y){ //Cria uma nova "imagem", com largura e altura especificadas
     FILE *fp = fopen("n.ppm", "w");
+    pixel **p;
+    dimx=x;
+    dimy=y;
     if(fp == NULL)
     {
 		printf("Erro na abertura do arquivo");
@@ -38,86 +44,53 @@ void image (int x,int y){ //Cria uma nova "imagem", com largura e altura especif
 	for(int j=0;j<y;j++)
 	{
 		for(int i=0;i<x;i++)
-		{
+		{   
+            p[i][j].red=255;
+		    p[i][j].green=255;
+		    p[i][j].blue=255;
             if(j!=dimx-1)
-            {            
-		            fprintf(fp,"255 255 255 ");;
+            {
+                fprintf(fp,"%d %d %d ",p[i][j].red,p[i][j].green,p[i][j].blue);
             }
 	        else
             {
-                    fprintf(fp,"255 255 255");;
+                fprintf(fp,"%d %d %d",p[i][j].red,p[i][j].green,p[i][j].blue);
             }
 		}
 		fprintf(fp,"\n");
 	}
         fclose(fp);
     }
+    return p;
 }
-
-void color(pixel p,unsigned short r, unsigned short g,unsigned short b){
-    p.red=r;
-    p.green=g;
-    p.blue=b;
-}
-
-void line(pixel **p,reta r){ //https://gist.github.com/mfilipelino/11240714#file-paintopengl-c
-    int dy = (r.p2.y - r.p1.y); //Diferença entre os pontos y2 e y1
-    int dx = (r.p2.x - r.p1.x); //Diferença entre os pontos x2 e x1
-    int sx,sy; //Variaveis que serão utilizadas para armazenar o sinal de x e y afim de saber o quadrante do segmento
-    int i=r.p1.y;
-    int j=r.p1.x;
-    if(r.p1.x<r.p2.x) //Determinar os sinais de x e y
+void save(pixel **p){
+    FILE *fp = fopen("n.ppm", "w");
+    if(fp == NULL)
     {
-        sx=1;
+	printf("Erro na abertura do arquivo");
     }
     else
     {
-        sx=-1;
-    }
-
-    if(r.p1.y<r.p2.y)
-    {
-        sy=1;
-    }
-    else
-    {
-        sy=-1;
-    }                  //fim da determinação de sinais
-    
-    int erro;
-    int erroTemp;
-    if(dx>dy) // Definição do erro inicial baseado na maior diferença
-    {
-        erro=dx/2;
-    }
-    else
-    {
-        erro=(-dy)/2;
-    }
-    
-    while(true)
+        fprintf(fp, "P3\n%d %d\n255\n", dimx, dimy);
+	for(int j=0;j<dimy;j++)
 	{
-	    color(p[i][j],255,255,255);
-        if((i==r.p2.y)&&(j==r.p2.x)) // Testa se chegou ao ponto final e encerra o algoritimo
-        {
-            break;
-        }
-
-        erroTemp=erro;
-
-        if(erroTemp > -dx)
-        {
-            erro -= dy;
-            j += sx;
-        }
-        
-        if(erroTemp < dy)
-        {
-            erro += dx;
-            i += sy;
-        }
+		for(int i=0;i<dimx;i++)
+		{
+            	    if(j!=dimx-1)
+            	    {            
+			fprintf(fp,"%d %d %d ",p[i][j].red,p[i][j].green,p[i][j].blue);
+            	    }
+	        else
+            {
+                    fprintf(fp,"%d %d %d",p[i][j].red,p[i][j].green,p[i][j].blue);
+            }
 	}
+		fprintf(fp,"\n");
+    }
+        fclose(fp);
+    }
 }
+
 
 /*void clear(pixel **p,unsigned short r, unsigned short g,unsigned short b){ //Limpa a imagem setando todos os pixels para uma cor especificada
     for(int j=0;j<dimy;j++)
@@ -129,6 +102,16 @@ void line(pixel **p,reta r){ //https://gist.github.com/mfilipelino/11240714#file
     }
 }
 
+
+void AllocMatrix(int N, int M, Area** a) {
+  int i;
+  a = malloc(N * sizeof(Area *));
+  for (i = 0; i < N; i++) {
+    a[i] = calloc(M, sizeof(Area*));
+  }
+}
+
+
 void rect(pixel **p){ //FUNÇÃO PARA DESENHAR RETÂNGULOS 
     for(i=___; i___; i++)
     {
@@ -139,7 +122,7 @@ void rect(pixel **p){ //FUNÇÃO PARA DESENHAR RETÂNGULOS
     }
 }*/
 
-
+#endif
 
 
 
--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 
 int main(){
     int x,y;
+    pixel **p;
+    reta r;
     printf("Digite o tamanho da imagem:\n");
     scanf("%d",&x);
     scanf("%d",&y);
-    image(x,y);
-    
-
+    p=image(x,y);
+    printf("Digite o x1/y1:\n");
+    scanf("%d",&r.p1.x);
+    scanf("%d",&r.p1.y);
+    printf("Digite o x2/y2:\n");
+    scanf("%d",&r.p2.x);
+    scanf("%d",&r.p2.y);
+    linha(p,r);
 return 0;
 }
