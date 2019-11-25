0bfeeeb89fee4b9be42a3057f23fd01471a4f5a1
Fernando Breno <fernandobrenofx@gmail.com>
2019-11-25 14:44:16 -0300

    Add files via upload

--- DrawTP.sh
+++ DrawTP.sh
@@ -1,4 +1,4 @@
-gcc `pkg-config --cflags gtk+-3.0` -o run main.c funcoes.c `pkg-config --libs gtk+-3.0`
+gcc `pkg-config --cflags gtk+-3.0` -o run main.c funcoes.c -Wall -Werror -g -std=c99 `pkg-config --libs gtk+-3.0`
 if [ $? -eq 0 ];then 
    echo "compilado com sucesso!"
     ./run
